cd9674b9814acd30e8adc2eb90e02c7596b3cfce
Islam Rustamov <islamrustamov@rambler.ru>
2025-01-19 01:06:20 +0300

    fix test

--- test/channels/room_channel_test.rb
+++ test/channels/room_channel_test.rb
@@ -2,6 +2,8 @@ require "test_helper"
 
 class RoomChannelTest < ActionCable::Channel::TestCase
   test "subscribes" do
+    stub_connection(current_player: Player.first)
+
     subscribe room_id: 15
 
     assert subscription.confirmed?
